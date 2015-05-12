26524d03fc94cbf1031bd23063a3095d57bea95e
shinda1020 <shinda1020@gmail.com>
2015-05-12 00:42:14 -0400

    Updated ICameraCtxUpdated

--- src/com/ctxengine/sensors/ICameraCtxUpdated.java
+++ src/com/ctxengine/sensors/ICameraCtxUpdated.java
@@ -4,9 +4,15 @@
 package com.ctxengine.sensors;
 
 /**
- * @author shinda
+ * This interface serves as the delegate to pass the camera sensor events to the
+ * context engine.
  * 
+ * @author Shinda
+ * @version 1.0 05/10/2015
  */
 public interface ICameraCtxUpdated {
+	/**
+	 * Implement this function to handle shaken event.
+	 */
 	void faceDetected();
 }
